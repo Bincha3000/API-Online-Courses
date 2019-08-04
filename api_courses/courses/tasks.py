ccdf61b5417b774571e20d4626ba754f92ebc697
Lancelot <bincha.1997@gmail.com>
2019-08-04 23:25:37 +0900

    email notifications

--- api_courses/courses/tasks.py
+++ api_courses/courses/tasks.py
@@ -1,16 +1,17 @@
+from datetime import date, timedelta
+
 from django.core.mail import send_mail
 
-
+from courses.models import Lesson
 
 
 def registration_email(first_name, last_name, email):
     subject = "Успешная регистрация"
     message = \
-    """
+        """
             Дорогой(ая) {first_name} {last_name}!
             Поздравляю вы успешно зарегестрировались!
     """.format(first_name=first_name, last_name=last_name)
-
 
     send_mail(
         subject=subject,
@@ -23,9 +24,16 @@ def registration_email(first_name, last_name, email):
 
 
 def notification_courses_email(lesson):
+
+    today = date.today()
+
+    lesons = Lesson.objects.filter(start_date=today).all()
+
+    students =
+
     subject = 'Напоминание о {}'.format(lesson.name)
     message = \
-    """
+        """
     Напоминаем о скором проведении урока по теме "{title}"!
     Дата и время проведения {date}
     """.format(title=lesson.title, date=lesson.date)
--- api_courses/users/views.py
+++ api_courses/users/views.py
@@ -1,4 +1,5 @@
 from courses.tasks import registration_email
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
