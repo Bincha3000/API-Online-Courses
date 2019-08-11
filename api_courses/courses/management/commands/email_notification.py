53e1d1a16f764ba543097fae6f51253bde809f5c
Lancelot <bincha.1997@gmail.com>
2019-08-11 22:49:26 +0900

    Update

--- api_courses/courses/management/commands/email_notification.py
+++ api_courses/courses/management/commands/email_notification.py
@@ -11,10 +11,10 @@ class Command(BaseCommand):
     def notification_scheduler_email(self, *args, **kwargs):
         scheduler = django_rq.get_scheduler('default')
         job = scheduler.schedule(
-            scheduled_time=datetime.utcnow(),  # Time for first execution, in UTC timezone
-            func=notification_courses_email,                     # Function to be queued         # Keyword arguments passed into function when executed
-            interval=60,                   # Time before the function is called again, in seconds
-            repeat=3,                     # Repeat this number of times (None means repeat forever)          # Arbitrary pickleable data on the job itself
+            scheduled_time=datetime.utcnow(),
+            func=notification_courses_email,
+            interval=60 * 60 * 24,
+            repeat=None,
         )
 
     def handle(self, *args, **kwargs):
