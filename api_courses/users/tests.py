151cb349990416da7dedb528b50c4827c4c56090
Lancelot <bincha.1997@gmail.com>
2019-09-09 23:46:34 +0300

    started testing the application

--- api_courses/users/tests.py
+++ api_courses/users/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('users:createuser')
+        data = {
+            'username': 'Testuser',
+            'first_name': 'Testfirst',
+            'last_name': 'Testsecond',
+            'email': 'test@twix.com',
+            'password': 'qwerty12'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # self.assertEqual(Account.objects.count(), 1)
+        # self.assertEqual(Account.objects.get().name, 'DabApps')
