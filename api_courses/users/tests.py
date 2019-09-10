ad4ea2672f840ff5132bcf0bf0b4666022091a88
Lancelot <bincha.1997@gmail.com>
2019-09-10 21:45:42 +0300

    Update accounts tests

--- api_courses/users/tests.py
+++ api_courses/users/tests.py
@@ -1,23 +1,52 @@
+import json
+
+
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
 from django.contrib.auth.models import User
 
 
 class AccountTests(APITestCase):
-    def test_create_account(self):
-        """
-        Ensure we can create a new account object.
-        """
-        url = reverse('users:createuser')
-        data = {
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.url = reverse('users:createuser')
+
+        cls.valid_data = {
             'username': 'Testuser',
             'first_name': 'Testfirst',
             'last_name': 'Testsecond',
-            'email': 'test@twix.com',
-            'password': 'qwerty12'
+            'email': 'test@bounty.com',
+            'password': 'qwerty123',
+            'confirm_password': 'qwerty321',
         }
-        response = self.client.post(url, data, format='json')
+
+        cls.pass_not_match = {
+            'username': 'username',
+            'password': 'qwerty123',
+            'confirm_password': 'qwerty321',
+            'email': 'test@sneakers.com'
+        }
+
+        cls.email_exists = {
+            'username': 'username',
+            'password': 'qwerty123',
+            'confirm_password': 'qwerty123',
+            'email': 'test@twix.com'
+        }
+
+        cls.client = APIClient()
+
+    def test_create_account(self):
+        """Ensure we can create a new account object."""
+        response = self.client.post(self.url, self.valid_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, 'Testuser')
+
+
+    def test_registration_invalid_data_passwords_doesnt_match(self):
+        """Make sure that the object is not created if the passwords do not match"""
+        response = self.client.post(self.url, self.pass_not_match, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
